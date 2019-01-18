0f4040722deb5a30e4a27aabce5a37849a780f49
rhildred <rhildred@gmail.com>
2019-01-18 11:50:31 -0500

    a few cleanups and comments

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from 'express';
-import bodyParser  from "body-parser";
-import Game  from "./Game";
 
 // Create a new express application instance
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+// be a regular web server from the www folder
 app.use(express.static("www"));
 
 app.get("/users/:uname", (req, res) => {
-    res.end("Hello " + req.params.uname);
+    // the user surfed to http://localhost:300/users/Rich
+    res.end("Greetings earthling " + req.params.uname + " ... Happy Friday!:)");
 });
 
+// try to figure out the port to use. In Heroku it is an an environment variable
 var port = process.env.PORT || parseInt(process.argv.pop()) || 3000;
 
-app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
+app.listen(port, () => {
+    // the application is now listening in the node event loop
+    console.log('Example app listening on port ' + port + '!');
+});
